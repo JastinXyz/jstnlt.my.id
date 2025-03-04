49ec4faf06484b60f17079925b360e1ce47c8225
JastinXyz <jastinlinggar@gmail.com>
2025-03-04 09:52:14 +0700

    fix: progressbar

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,16 +2,13 @@
 
 import type { Session } from "next-auth"
 import { SessionProvider } from "next-auth/react"
-import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import Next13ProgressBar from "next13-progressbar";
 
 export default function Providers({ session, children }: { session: Session | null, children: React.ReactNode }) {
     return (
         <SessionProvider session={session}>
             {children}
-            <ProgressBar
-                height="4px"
-                color="#7993B7"
-            />
+            <Next13ProgressBar color="#7993B7" />
         </SessionProvider>
     )
 }
